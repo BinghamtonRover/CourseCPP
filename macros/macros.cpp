4e7555d9a9adb68334bedb157738ad95dd43935e
krmnino <kurt.manrique.n@gmail.com>
2021-04-24 14:13:38 -0400

    added macros examples

---
+++ macros/macros.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+#define GRAVITY 9.81
+#define MTRX std::vector<std::vector<int>>
+
+int main(){
+    std::cout << GRAVITY << std::endl;
+    MTRX m;
+}
+
+#undef GRAVITY
---
+++ macros/why.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define X int factorial(int n){
+#define XX int acc = 1;
+#define XXX for(int i = 1; i <= n; i++){
+#define XXXX acc *= i;
+#define XXXXX printf("%d\n", acc);
+#define XXXXXX }
+#define XXXXXXX return acc;
+#define XXXXXXXX }
+
+#define Y factorial(
+#define YY );
+
+//int factorial(int n){
+//    int acc = 1;
+//    for(int i = 1; i <= n; i++){
+//        acc *= i;
+//        printf("%d\n", acc);
+//    }
+//    return acc;
+//}
+
+X
+XX
+XXX
+XXXX
+XXXXX
+XXXXXX
+XXXXXXX
+XXXXXXXX
+
+
+int main(){
+    int ret = Y 6 YY
+    //int ret = factorial(6);
+    printf("ret = %d", ret);
+}
+
+#undef GRAVITY
