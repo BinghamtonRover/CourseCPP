9396118c20c9db2e34e51839ef1e903763ed6e7e
krmnino <kurt.manrique.n@gmail.com>
2021-04-24 15:50:34 -0400

    added source files examples

---
+++ source_files/car.hpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class Car{
+private:
+    float length;
+    int year;
+    const char* brand;
+public:
+    Car(int, const char*, float);
+    ~Car();
+    int get_year();
+    float get_length();
+    const char* get_brand();
+};
